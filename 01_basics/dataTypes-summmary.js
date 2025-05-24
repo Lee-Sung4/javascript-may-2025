2bc8d8bc66487dde3a3684b082769d51db53ab1d
Lee-Sung4 <tungjlee4@gmail.com>
2025-05-24 23:40:34 +0530

    summary or intro to datatypes

---
+++ 01_basics/dataTypes-summmary.js
@@ -0,0 +1,23 @@
+// DataType
+
+
+
+/*  # Primitive( call by value)
+
+ 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+********* Symbol to be explored****
+
+
+
+ Reference (Non primitive)
+
+ : Array, Object, Functions
+
+
+
+*******to be explored*****
+
+ */
+
+//***************  JavaScript is a dynamically typed language ***************************
