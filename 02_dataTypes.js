c3440f66bba6570e2cf7121564e34329d66d7050
Lee-Sung4 <tungjlee4@gmail.com>
2025-05-24 23:33:37 +0530

    datatypes
    add datatypes learning file from local

---
+++ 02_dataTypes.js
@@ -0,0 +1,29 @@
+console.log("dataTypes");
+
+
+/* Documentation => MDN - https://developer.mozilla.org/en-US/docs/Web/API/Window
+                    ecma - https://tc39.es/ecma262/multipage/#sec-intro
+*/
+
+// Datatypes
+
+let name = "lee sung";
+let age  = 22;
+let isLoggedIn = true;
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ('') (``)  ("")
+// boolean => true/false
+// null => standalone value (object)
+// undefine => 
+// symbol => unique
+
+
+console.log(typeof name);
+
+console.log( typeof age, typeof isLoggedIn, typeof state, typeof null);
+
+
