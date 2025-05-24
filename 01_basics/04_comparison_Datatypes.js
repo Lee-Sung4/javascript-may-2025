11aac8ec5be3c24e252e11887706f4afbfe6db8e
Lee-Sung4 <tungjlee4@gmail.com>
2025-05-24 23:39:26 +0530

    Comparison of datatypes

---
+++ 01_basics/04_comparison_Datatypes.js
@@ -0,0 +1,21 @@
+// comparison of different data type
+
+// check
+console.log(2>2);
+
+console.log("3">2);
+console.log("02"== 2);
+
+// strict check
+
+// check the dataTyope as well
+
+// "2" === 2
+ 
+// ===     (equal to same datatype)
+// !==
+// <==
+// >==
+
+
+
