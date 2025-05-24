eea7916a8025b29f0326419c0e27aaff961c9fed
Lee-Sung4 <tungjlee4@gmail.com>
2025-05-24 23:37:20 +0530

    Conversion_Operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,96 @@
+// conversion 
+
+let cgpa = "8.9";
+
+let valueInNumber = Number(cgpa)
+console.log(typeof valueInNumber);
+console.log(valueInNumber + 11);
+
+
+let searchId = "123abc";
+
+let idToNumber = Number(searchId);
+console.log(idToNumber);
+
+
+let logIn = true;
+let logOut = false;
+
+logIn = Number(logIn)
+logOut = Number(logOut)
+
+console.log(logIn);
+console.log(logOut);
+
+
+// "8.9" => 8.9
+// "123abc" => NaN
+// true => 1; false => 0
+
+let isActive = 1;
+let valueToBoolean = Boolean(valueInNumber)
+
+console.log(valueToBoolean);
+
+
+
+// true => 1; false => 0
+// "" => false 
+// "lee" => true
+
+// also the same for string it can also be converted 
+
+
+
+//*********************************************** Operations ***************************************************
+
+let value = 12;
+// let negativeValue = - value;
+// console.log(negativeValue);
+
+// console.log(3 + 2);
+// console.log(3 - 2);
+// console.log(3 * 2);
+// console.log(3 / 2);
+// console.log(3 ** 2);
+// console.log(3 % 2);                         use alot
+
+
+//******************************************* string *****************************************
+
+let str1 = "Lee";
+let str2 = " Sung"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("2" + 2);
+console.log("2" + 2);
+console.log("2" + 2 + 2);
+console.log(2 + 2 + "2");
+// not recommended
+// use parenthesis
+console.log((2 + 3) * 2 % 5)
+
+
+//****************** prefix (++x) and postfix(x++)******************
+
+// let x = 3;
+// const y = x++;
+
+// console.log(`x:${x}, y:${y}`);
+// // Expected output: "x:4, y:3"
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
