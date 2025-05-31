cc9e724288da0399e3be831e9643f6c583d77f91
Lee-Sung4 <tungjlee4@gmail.com>
2025-05-31 23:53:54 +0530

    Add files via upload

---
+++ DOM/01_script.js
@@ -0,0 +1,25 @@
+// DOM Manipulation
+// selecting with Id
+ const submitButton = document.getElementById("submitButton");
+ submitButton.style.backgroundColor = "gray";
+
+//  console.log(submitButton);
+ 
+
+
+// selecting with ClassName
+// return html collection
+// kinda simillar to array
+//  can access with index
+const boxes = document.getElementsByClassName("box");
+
+console.log(boxes);
+boxes[0].style.backgroundColor = "green";
+boxes[2].style.backgroundColor = "red";
+boxes[2].style.backgroundColor = "red";
+
+
+
+
+
+
